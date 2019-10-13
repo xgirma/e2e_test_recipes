a29b995c639587cd3c4e5b656a69155cbc793e07
Girma Nigusse <xgirma@gmail.com>
2019-10-12 20:25:09 -0400

    add eslint

--- configuration/protractor/protractor-commonjs/spec/search.spec.js
+++ configuration/protractor/protractor-commonjs/spec/search.spec.js
@@ -2,22 +2,22 @@ const protractor = require('protractor');
 const assert = require('assert');
 
 describe('google search', () => {
-    it('should be on google search page', async () => {
-        browser.waitForAngularEnabled(false);
-        browser.get("https://www.google.com/");
-        const title = await browser.getTitle();
-        assert(title, 'Google');
-    });
+  it('should be on google search page', async () => {
+    browser.waitForAngularEnabled(false);
+    browser.get('https://www.google.com/');
+    const title = await browser.getTitle();
+    assert(title, 'Google');
+  });
 
-    it('should search for Cheese!', () => {
-        const searchBox = element(by.name('q'));
-        assert.ok(searchBox.isDisplayed());
-        searchBox.sendKeys('Cheese!', protractor.Key.ENTER);
-    });
+  it('should search for Cheese!', () => {
+    const searchBox = element(by.name('q'));
+    assert.ok(searchBox.isDisplayed());
+    searchBox.sendKeys('Cheese!', protractor.Key.ENTER);
+  });
 
-    it('the page title should start with "Cheese!"', async () => {
-        const title = await browser.getTitle();
-        const isTitleStartWithCheese = title.lastIndexOf(`Cheese!`, 0) === 0;
-        assert.ok(isTitleStartWithCheese);
-    });
+  it('the page title should start with "Cheese!"', async () => {
+    const title = await browser.getTitle();
+    const isTitleStartWithCheese = title.lastIndexOf('Cheese!', 0) === 0;
+    assert.ok(isTitleStartWithCheese);
+  });
 });
