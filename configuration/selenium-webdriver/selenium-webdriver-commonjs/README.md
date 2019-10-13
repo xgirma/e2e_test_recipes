1f8c1889ef7172d4771eafc59a0c1556b1b2be86
Girma Nigusse <xgirma@gmail.com>
2019-10-12 21:34:56 -0400

    selenium-webdriver

---
+++ configuration/selenium-webdriver/selenium-webdriver-commonjs/README.md
@@ -0,0 +1,13 @@
+# selenium-webdriver: commonjs & jasmine
+
+    module: commonjs
+    
+    framework: selenium-webdriver
+    
+    assertion: jasmine
+
+## To run your test
+
+    npm install
+    
+    npm run test
---
+++ configuration/selenium-webdriver/selenium-webdriver-commonjs/spec/search.spec.js
@@ -0,0 +1,32 @@
+const {Builder, By, Key} = require('selenium-webdriver');
+require('chromedriver');
+
+describe('google Search', function() {
+  let browser;
+
+  beforeAll( async function() {
+    browser = await new Builder().forBrowser('chrome').build();
+    browser.get('http://www.google.com');
+  });
+
+  it('should be on google search page', async () => {
+    const title = await browser.getTitle();
+    expect(title).toEqual('Google');
+  });
+
+  it('should search for Cheese!', async function () {
+    const searchBox = await browser.findElement(By.name('q'));
+    expect(await searchBox.isDisplayed()).toBe(true);
+    searchBox.sendKeys('Cheese!', Key.ENTER);
+  });
+
+  it('the page title should start with "Cheese!"', async () => {
+    const title = await browser.getTitle();
+    const isTitleStartWithCheese = title.lastIndexOf('Cheese!', 0) === 0;
+    expect(isTitleStartWithCheese).toBe(true);
+  });
+
+  afterAll(function() {
+    browser.quit();
+  });
+});
