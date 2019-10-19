36c60f31c769899e28e0807027e2e0dc473c3eba
Girma Nigusse <xgirma@gmail.com>
2019-10-18 21:30:42 -0400

    protractor ES6 module babel jasmine

--- configuration/protractor/protractor-ES6-module-babel-jasmine/protractor.conf.js
+++ configuration/protractor/protractor-ES6-module-babel-jasmine/protractor.conf.js
@@ -1,17 +1,18 @@
 exports.config = {
-    directConnect: true,
-    specs: [
-        'spec/*.spec.js'
-    ],
-    capabilities: {
-        browserName: 'chrome',
-    },
-    framework: 'jasmine',
-    jasmineNodeOpts: {
-        isVerbose: true,
-        realtimeFailure: true
-    },
-    onPrepare: () => {
-        require("@babel/register");
-    }
+  directConnect: true,
+  specs: [
+    'spec/*.spec.js',
+  ],
+  capabilities: {
+    browserName: 'chrome',
+  },
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    isVerbose: true,
+    realtimeFailure: true,
+  },
+  onPrepare: () => {
+    /* eslint-disable-next-line global-require */
+    require('@babel/register');
+  },
 };
