1720edfcda7725a3f4e67bc851d1896af6ec8977
Girma Nigusse <xgirma@gmail.com>
2019-10-18 21:17:23 -0400

    protractor commonjs mocha

--- configuration/protractor/protractor-commonjs-mocha/protractor.conf.js
+++ configuration/protractor/protractor-commonjs-mocha/protractor.conf.js
@@ -1,14 +1,12 @@
 exports.config = {
-    directConnect: true,
-    specs: [
-        'spec/*.spec.js'
-    ],
-    capabilities: {
-        browserName: 'chrome',
-    },
-    framework: 'mocha',
-    mochaOpts: {
-        reporter: "spec",
-        slow: 3000
-    }
+  directConnect: true,
+  specs: ["spec/*.spec.js"],
+  capabilities: {
+    browserName: "chrome"
+  },
+  framework: "mocha",
+  mochaOpts: {
+    reporter: "spec",
+    slow: 3000
+  }
 };
--- configuration/protractor/protractor-commonjs-mocha/spec/search.spec.js
+++ configuration/protractor/protractor-commonjs-mocha/spec/search.spec.js
@@ -1,26 +1,26 @@
-const protractor = require('protractor');
-const assert = require('assert');
+const protractor = require("protractor");
+const assert = require("assert");
 
-describe('google search', () => {
+describe("google search", () => {
   before(() => {
     browser.waitForAngularEnabled(false);
-    browser.get('https://www.google.com/');
+    browser.get("https://www.google.com/");
   });
 
-  it('should be on google search page', async () => {
+  it("should be on google search page", async () => {
     const title = await browser.getTitle();
-    assert(title, 'Google');
+    assert(title, "Google");
   });
 
-  it('should search for Cheese!', async () => {
-    const searchBox = await element(by.name('q'));
+  it("should search for Cheese!", async () => {
+    const searchBox = await element(by.name("q"));
     assert.ok(searchBox.isDisplayed());
-    searchBox.sendKeys('Cheese!', protractor.Key.ENTER);
+    searchBox.sendKeys("Cheese!", protractor.Key.ENTER);
   });
 
   it('the page title should start with "Cheese!"', async () => {
     const title = await browser.getTitle();
-    const isTitleStartWithCheese = title.lastIndexOf('Cheese!', 0) === 0;
+    const isTitleStartWithCheese = title.lastIndexOf("Cheese!", 0) === 0;
     assert.ok(isTitleStartWithCheese);
   });
 });
