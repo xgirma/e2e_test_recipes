a7a385f78f89031fe9d626a4f439c4b2dc09a5ee
Girma Nigusse <xgirma@gmail.com>
2019-10-21 20:24:29 -0400

    rename folder

---
+++ configuration/webdriverio/webdriverio-commonjs-cucumber-assert/wdio.conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+  runner: "local",
+  path: "/",
+  specs: ["./features/*.feature"],
+  capabilities: [{
+      browserName: "chrome"
+    }
+  ],
+  services: ["chromedriver"],
+  logLevel: "silent",
+  framework: "cucumber",
+  cucumberOpts: {
+    require: ['./features/step_definitions/stepdefs.js'],
+    timeout: 60000,
+  },
+  reporters: ["dot"],
+  before: function() {
+    const chai    = require('chai');
+    global.expect = chai.expect;
+  },
+};
