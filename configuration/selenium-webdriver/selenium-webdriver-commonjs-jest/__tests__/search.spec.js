2bbc6cb202cecfcf96402e68db0a496fd48fd6c3
Girma Nigusse <xgirma@gmail.com>
2019-10-28 19:00:27 -0400

    selenium-webdriver commonjs jest

--- configuration/selenium-webdriver/selenium-webdriver-commonjs-jest/__tests__/search.spec.js
+++ configuration/selenium-webdriver/selenium-webdriver-commonjs-jest/__tests__/search.spec.js
@@ -13,7 +13,7 @@ describe("google Search", () => {
     browser.quit();
   });
 
-  it("should be on google search page", async () => {
+  test("should be on google search page", async () => {
     const searchBox = await browser.findElement(By.name("q"));
     await browser.wait(until.elementIsVisible(searchBox), 5000);
 
@@ -21,13 +21,13 @@ describe("google Search", () => {
     expect(title).toEqual("Google");
   });
 
-  it("should search for Cheese!", async () => {
+  test("should search for Cheese!", async () => {
     const searchBox = await browser.findElement(By.name("q"));
     expect(await searchBox.isDisplayed()).toBe(true);
     searchBox.sendKeys("Cheese!", Key.ENTER);
   });
 
-  it('the page title should start with "Cheese!"', async () => {
+  test('the page title should start with "Cheese!"', async () => {
     await browser.wait(until.urlContains("search"), 5000);
 
     const title = await browser.getTitle();
